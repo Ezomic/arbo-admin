8b70e8a0c19680a3a983e63a3143daa7e3384ebe
Robbin Thijssen <robbin_thijssen@hotmail.nl>
2026-07-03 22:07:15 +0200

    feat: contract type management and internal API

---
+++ app/Http/Controllers/ContractTypeController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ContractType;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+use Inertia\Response;
+
+class ContractTypeController extends Controller
+{
+    public function index(): Response
+    {
+        return Inertia::render('contract-types/Index', [
+            'contractTypes' => ContractType::query()->latest()->get(),
+        ]);
+    }
+
+    public function store(Request $request): RedirectResponse
+    {
+        $data = $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'description' => ['nullable', 'string'],
+        ]);
+
+        ContractType::query()->create($data);
+
+        return to_route('contract-types.index');
+    }
+
+    public function update(Request $request, ContractType $contractType): RedirectResponse
+    {
+        $data = $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'description' => ['nullable', 'string'],
+            'is_active' => ['boolean'],
+        ]);
+
+        $contractType->update($data);
+
+        return to_route('contract-types.index');
+    }
+}
