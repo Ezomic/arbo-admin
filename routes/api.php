ff9d0798e93c78299405bb1ab2e57755ce83eebd
Robbin Thijssen <robbin_thijssen@hotmail.nl>
2026-07-03 23:05:14 +0200

    feat: session security and API rate limiting (ARBO-15, ARBO-17)

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\Api\ContractTypeApiController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['auth:api-client'])->group(function () {
+Route::middleware(['auth:api-client', 'throttle:60,1'])->group(function () {
     Route::get('contract-types', [ContractTypeApiController::class, 'index'])
         ->middleware('ability:contract-types:read');
 });
